025195e1f9220b239593103847392a032c9b4e00
Владислав Горохов <gbg.0101@mail.ru>
2019-06-27 11:11:42 +0600

    final version

--- HttpWebRequestHomeWork/MainWindow.xaml.cs
+++ HttpWebRequestHomeWork/MainWindow.xaml.cs
@@ -4,16 +4,19 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace HttpWebRequestHomeWork
 {
@@ -26,6 +29,7 @@ namespace HttpWebRequestHomeWork
 
         private void ButtinClick(object sender, RoutedEventArgs e)
         {
+            dataTextBox.Text = "";
             string urlAddress = urlTextBox.Text;
 
             HttpWebRequest request = WebRequest.CreateHttp(urlAddress);
@@ -47,7 +51,27 @@ namespace HttpWebRequestHomeWork
 
                 string data = readStream.ReadToEnd();
 
-                dataTextBox.Text = data;
+                string tag = tagTextBox.Text;
+                string pattern = string.Format(@"\<{0}.*?\>(?<tegData>.+?)\<\/{0}\>", tag.Trim());
+
+                Regex regex = new Regex(pattern, RegexOptions.ExplicitCapture);//перечисления
+                MatchCollection matches = regex.Matches(data);
+
+                List<string> list = new List<string>();
+
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    Regex reg = new Regex("<[^>]+>", RegexOptions.IgnoreCase);
+
+                    list.Add(reg.Replace(matches[i].Groups["tegData"].Value, ""));
+                }
+
+                foreach (var item in list)
+                {
+                    dataTextBox.Text += item + "\r\n";
+                }
+
+                //dataTextBox.Text = clearData;
 
                 response.Close();
                 readStream.Close();
